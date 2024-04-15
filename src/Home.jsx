e5440726ac950ef8924e03fc48a0c542bc8558b0
pradhansumitXD <np03cs4a220215@heraldcollege.edu.np>
2024-04-15 18:16:48 +0545

    added booking-popup

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,14 @@ import Header from './components/Header/Header';
 import AboutUs from './components/AboutUs/AboutUs';
 import Footer from './components/Footer/Footer';
 import CarSlide from './components/CarSlide/CarSlide';
+import Booking from './components/Booking/Booking';
+
 
 const Home = () => {
   return (
     <div className='page-container'>
       <Header />
+      <Booking />
       <AboutUs />
       <CarSlide />
       <Footer />
