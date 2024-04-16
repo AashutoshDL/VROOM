81b0481aa9c8f32f04eab335a513e6aabfacfa9b
pradhansumitXD <np03cs4a220215@heraldcollege.edu.np>
2024-04-16 07:47:02 +0545

    added vehicle-listing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
-
+import Vehicles from './components/Vehicles/Vehicles';
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/vehicles" element={<Vehicles />} />
         </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App; 
